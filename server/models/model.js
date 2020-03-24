ff6a8c0f2edeeb30ca675167821c83352d1c995e
dlwong <starletfordorris@gmail.com>
2020-03-23 23:24:06 -0700

    Clean files

--- server/models/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const playerSchema = new mongoose.Schema({
-  email: String
-});
-
-const Player = mongoose.model('Player', playerSchema);
-
-module.exports = Player;
